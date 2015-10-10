4547fc679f52aff54e36835e76a095404c3ba43a
Kelpflakes <Kelpflakes@users.noreply.github.com>
2015-10-10 01:56:53 -0700

    server made

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ var express = require("express"),
     router = express.Router(),
     port = process.env.PORT || 8080;
 
+app.use(express.static('public'));
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+
+router.post('/hello', function(req, res) {   
+    //res.json({ message: 'hooray! welcome to our api!' });    
 });
 
-app.use('/api', router);
+router.get('/hello', function(req, res) {   
+    res.json({ message: 'hooray! welcome to our api!' });    
+});
 
+app.use('/result', router);
 app.listen(port);
 console.log('Magic happens on port ' + port);
